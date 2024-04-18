7553a4fe6a0d2326ef8563a52a8e0abaaf347895
Lyudmila Tuzova <ltuzova3@gatech.edu>
2024-04-18 16:51:20 +0000

    unipelt experiments

--- classification_training.py
+++ classification_training.py
@@ -13,7 +13,9 @@ from datasets import load_dataset
 
 from transformers import AutoTokenizer, DataCollatorWithPadding, EarlyStoppingCallback
 from transformers import TrainingArguments
+from adapters import AdapterTrainer
 from transformers import AutoConfig
+from adapters import AutoAdapterModel
 
 def compute_metrics(pred, average = 'macro'):
     labels = pred.label_ids
@@ -32,9 +34,10 @@ def compute_metrics(pred, average = 'macro'):
     
     
 def run_classifier(output_dir, classification_task = 'imdb', pretrained_model = 'roberta-base', \
- torch_compile = True,train = True, evaluate_on_test = True):
+ torch_compile = True,train = True, evaluate_on_test = True, adapter_hub_name = None, learning_rate=2e-5):
     print("Early stopping with best val on f1 macro")
-    print("Classification task: {0}, pretrained model: {1}".format(classification_task, pretrained_model))
+    print("Classification task: {0}, pretrained model: {1}, lr: {2}".format(classification_task, pretrained_model,
+    learning_rate))
     
     if classification_task == 'helpfulness':
         dataset = load_dataset("BigTMiami/amazon_helpfulness")
@@ -58,21 +61,38 @@ def run_classifier(output_dir, classification_task = 'imdb', pretrained_model = 
 
     # Set Classifier Settings
     classification_config = AutoConfig.from_pretrained('roberta-base')
-    classification_config.classifier_dropout = 0.1 # From Paper
-    classification_config.num_of_labels = 2
-    classification_config.id2label=id2label
-    classification_config.label2id=label2id
+    if not adapter_hub_name:
+      classification_config.classifier_dropout = 0.1 # From Paper
+      classification_config.num_of_labels = 2
+      classification_config.id2label=id2label
+      classification_config.label2id=label2id
         
     print("Classification config: {0}".format(classification_config))
     
-    model = AutoModelForSequenceClassification.from_pretrained(pretrained_model,  config=classification_config)
+    if not adapter_hub_name:
+      model = AutoModelForSequenceClassification.from_pretrained(pretrained_model,  config=classification_config)
+    else:
+      model = AutoAdapterModel.from_pretrained(pretrained_model,  config=classification_config)
+
+    if adapter_hub_name:
+      loaded_adapter_name = model.load_adapter(adapter_hub_name, with_head=False)
+      model.add_classification_head(loaded_adapter_name, num_labels=2, id2label=id2label)
+
+      # Activate the adapter
+      model.set_active_adapters(loaded_adapter_name) 
+
+      # Set the adapter to be used for training
+      model.train_adapter(loaded_adapter_name)
+
+      summary = model.adapter_summary()
+      print(summary)
 
     training_args = TrainingArguments(
         output_dir=output_dir,
         overwrite_output_dir=True,
         evaluation_strategy="epoch",
         save_strategy="epoch",
-        learning_rate=2e-5, # Paper: this is for Classification, not domain training
+        learning_rate=learning_rate, # Paper: this is for Classification, not domain training
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
         # Lyudmila: changed from 3 to 10 -> used for small roberta
@@ -100,16 +120,28 @@ def run_classifier(output_dir, classification_task = 'imdb', pretrained_model = 
     early_stopping = EarlyStoppingCallback(early_stopping_patience=3) # from paper
     # callbacks=[early_stopping],
     
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=dataset["train"],
-        eval_dataset=dataset["dev"],
-        tokenizer=tokenizer,
-        data_collator=data_collator,
-        compute_metrics=compute_metrics,
-        callbacks=[early_stopping]
-    )
+    if not adapter_hub_name:
+      trainer = Trainer(
+          model=model,
+          args=training_args,
+          train_dataset=dataset["train"],
+          eval_dataset=dataset["dev"],
+          tokenizer=tokenizer,
+          data_collator=data_collator,
+          compute_metrics=compute_metrics,
+          callbacks=[early_stopping]
+      )
+    else:
+      trainer = AdapterTrainer(
+          model=model,
+          args=training_args,
+          train_dataset=dataset["train"],
+          eval_dataset=dataset["dev"],
+          tokenizer=tokenizer,
+          data_collator=data_collator,
+          compute_metrics=compute_metrics,
+          callbacks=[early_stopping]
+      )      
     
     print("Trainer args: {0}".format(trainer.args))
     
@@ -126,4 +158,4 @@ def run_classifier(output_dir, classification_task = 'imdb', pretrained_model = 
       test_evaluation_result = trainer.evaluate(dataset["dev"])
       print("Dev set eval: {0}".format(test_evaluation_result))
     
-    return trainer
+    return trainer, model
